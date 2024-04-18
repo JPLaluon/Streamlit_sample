f4268c6b85a8de5ab4d83b3006d92f4296e78158
JPLaluon <167404080+JPLaluon@users.noreply.github.com>
2024-04-18 18:31:19 +0800

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -1,84 +1,51 @@
-import page
-
 import streamlit as st
 
-def main():
-    # Define the sidebar navigation
-    page = st.sidebar.selectbox("Select a page", ["Home", "Information"])
-
-    # Program Proper - Home Page
-    if page == "Home":
-        st.header('Concrete House Maintenance Analyzer')
-        st.write('Click below to start analyzing.')
-
-    st.title('Concrete House Maintenance Analyzer')
-    st.write('Welcome to Concrete House Maintenance Analyzer! '
-             'This program will help you identify the parts of your '
-             'house that either need maintenance or not. If it needs maintenance, '
-             'you will be given numerical scalar measurements for each component of '
-             'the building to guide you through the whole process within this service. '
-             'If not, then an approximation of how long does it take before the next '
-             'maintenance will be provided based on the information that’s been provided.')
-
-    # Button to navigate to the information page
+# Home Page
+def home():
+    st.title("Welcome to Survey App")
+    st.write("This is a survey about your experience.")
     if st.button("Start"):
-        st.experimental_rerun()
+        st.session_state['current_page'] = 'questions'
+        st.session_state['current_question'] = 0
+        st.session_state['responses'] = []
 
-
-# Function to display a single question with five-button Likert scale
-def display_question(question_number, question):
+# Questions Page
+def questions():
     st.title("Answer the Questions")
-    st.subheader(f"Question {question_number}: {question}")
-    return st.radio("", options=[1, 2, 3, 4, 5], index=2, key=f"question_{question_number}")
-
-
-def main():
-    st.title("Welcome to My Website")
-    st.session_state['current_question'] = 0
-    st.session_state['responses'] = []
-
-    # Button to lead to new interface
-    if st.button("Start"):
-        if st.session_state['current_question'] < len(questions):
-            response = display_question(st.session_state['current_question'] + 1,
-                                        questions[st.session_state['current_question']])
-            st.session_state['responses'].append(response)
+    questions = [
+        "How satisfied are you with the product quality?",
+        "How likely are you to recommend our service to others?",
+        "How easy was it to use our website?"
+    ]
+    if st.session_state['current_question'] < len(questions):
+        st.subheader(f"Question {st.session_state['current_question'] + 1}: {questions[st.session_state['current_question']]}")
+        response = st.radio("", options=[1, 2, 3, 4, 5], index=2, key=f"question_{st.session_state['current_question']}")
+        st.session_state['responses'].append(response)
+        if st.button("Next"):
             st.session_state['current_question'] += 1
+    else:
+        st.write("All questions answered.")
+        st.button("See Results", key="result")
+        st.session_state['current_page'] = 'result'
 
-        if st.session_state['current_question'] == len(questions):
-            st.write("All questions answered. Your responses:")
-            for i, response in enumerate(st.session_state['responses']):
-                st.write(f"Question {i + 1}: {response}")
-        else:
-            if st.button("Next"):
-                response = display_question(st.session_state['current_question'] + 1,
-                                            questions[st.session_state['current_question']])
-                st.session_state['responses'].append(response)
-                st.session_state['current_question'] += 1
+# Result Page
+def result():
+    st.title("Survey Results")
+    st.write("Your responses:")
+    for i, response in enumerate(st.session_state['responses']):
+        st.write(f"Question {i+1}: {response}")
 
-# Information page
-    elif page == "Information":
-        st.header("Information Interface")
-        st.write("This is the information interface.")
-        st.write("New information goes here.")
+# Main function to control page flow
+def main():
+    if 'current_page' not in st.session_state:
+        st.session_state['current_page'] = 'home'
 
-    # Projects section
-    st.header('Projects')
+    if st.session_state['current_page'] == 'home':
+        home()
+    elif st.session_state['current_page'] == 'questions':
+        questions()
+    elif st.session_state['current_page'] == 'result':
+        result()
 
-    project_1 = {
-        'title': 'Project 1',
-        'description': 'Description of project 1.',
-        'github_link': 'https://github.com/example/project1',
-        'demo_link': 'https://demo.project1.com'
-    }
-
-    projects = [project_1]
-
-    for project in projects:
-        st.subheader(project['title'])
-        st.write(project['description'])
-        st.write('GitHub: [{}]({})'.format(project['title'], project['github_link']))
-        st.write('Demo: [{}]({})'.format(project['title'], project['demo_link']))
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
